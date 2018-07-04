1744c667c40bd678cbb3c37732fb79dcbc369950
Kenny <kennybrown11@hotmail.com>
2018-07-04 19:43:24 +0000

    working characterCounter function

--- characterCounting.js
+++ characterCounting.js
@@ -1,1 +1,16 @@
-// Create a function that returns all the unique characters passed into the function
+function countLetters(string) {
+  var str = string.split(" ").join("");
+  var letters = {}
+    for (let i = 0; i < str.length; i++) {
+    if (!letters[str.charAt(i)]){
+      letters[str.charAt(i)] = 1;
+    } else {
+      var increment = 1;
+      letters[str.charAt(i)] += increment;
+    }
+  }
+ 
+  return letters;
+ }
+ 
+ console.log(countLetters("lighthouse in the house"));
